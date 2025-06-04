17ff1c1bc7b53230f601e68c216c55ee0a4d7120
Rekvim <151747849+Rekvim@users.noreply.github.com>
2025-06-04 08:35:24 +0300

    main

--- OtherTestSettings.cpp
+++ OtherTestSettings.cpp
@@ -7,14 +7,19 @@ OtherTestSettings::OtherTestSettings(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    ui->pushButton_change_value->setEnabled(false);
+    ui->pushButton_delete_value->setEnabled(false);
+    ui->pushButton_change_step->setEnabled(false);
+    ui->pushButton_delete_step->setEnabled(false);
+
     connect(ui->listWidget_value, &QListWidget::currentRowChanged, this, [=](int v) {
         ui->pushButton_change_value->setEnabled(v >= 0);
-        ui->pushButton_delete_value->setEnabled((v >= 0) & (ui->listWidget_value->count() > 1));
+        ui->pushButton_delete_value->setEnabled((v >= 0) && (ui->listWidget_value->count() > 1));
     });
 
     connect(ui->pushButton_add_value, &QPushButton::clicked, this, [=]() {
         ui->listWidget_value->addItem("50.0");
-        ui->pushButton_delete_value->setEnabled(ui->listWidget_value->currentRow() >= 0);
+        ui->listWidget_value->setCurrentRow(ui->listWidget_value->count() - 1);
     });
 
     connect(ui->pushButton_delete_value, &QPushButton::clicked, this, [=]() {
@@ -45,12 +50,12 @@ OtherTestSettings::OtherTestSettings(QWidget *parent) :
 
     connect(ui->listWidget_step, &QListWidget::currentRowChanged, this, [=](int v) {
         ui->pushButton_change_step->setEnabled(v >= 0);
-        ui->pushButton_delete_step->setEnabled((v >= 0) & (ui->listWidget_step->count() > 1));
+        ui->pushButton_delete_step->setEnabled((v >= 0) && (ui->listWidget_step->count() > 1));
     });
 
     connect(ui->pushButton_add_step, &QPushButton::clicked, this, [=]() {
         ui->listWidget_step->addItem("3.0");
-        ui->pushButton_delete_step->setEnabled(ui->listWidget_step->currentRow() >= 0);
+        ui->listWidget_step->setCurrentRow(ui->listWidget_step->count() - 1);
     });
 
     connect(ui->pushButton_delete_step, &QPushButton::clicked, this, [=]() {
@@ -65,16 +70,14 @@ OtherTestSettings::OtherTestSettings(QWidget *parent) :
 
     connect(ui->pushButton_change_step, &QPushButton::clicked, this, [=]() {
         bool ok;
-        double d = QInputDialog::getDouble(
-            this,
-            "Ввод числа",
-            "Значение:",
-            ui->listWidget_step->currentItem()->text().toDouble(),
-            0.0,
-            100.0,
-            1,
-            &ok
-        );
+        double d = QInputDialog::getDouble(this,
+                                           "Ввод числа",
+                                           "Значение:",
+                                           ui->listWidget_step->currentItem()->text().toDouble(),
+                                           0.0,
+                                           100.0,
+                                           1,
+                                           &ok);
 
         if (ok) {
             ui->listWidget_step->currentItem()->setText(QString::asprintf("%.1f", d));
