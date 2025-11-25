8a857f5690df664f4b62e42d85e1e831630df7dc
Rekvim <151747849+Rekvim@users.noreply.github.com>
2025-11-25 11:58:07 +0300

    main

--- Src/Runners/BaseRunner.h
+++ Src/Runners/BaseRunner.h
@@ -24,15 +24,7 @@ public:
     ~BaseRunner() override;
 
 public slots:
-<<<<<<< Updated upstream
-    void start() final;   // вызывает buildConfig(), заводит поток, вешает общие connect’ы
-    void stop() override; // дергает worker->StoppingTheTest()
-    void releaseBlock() final override;
 
-protected:
-    virtual RunnerConfig buildConfig() = 0;          // собрать worker + задать totalMs/график
-    virtual void wireSpecificSignals(Test& t) {}     // доцепить спец. сигналы текущего теста
-=======
     void start() final;
     void stop() override;
     void releaseBlock() final override;
@@ -40,7 +32,6 @@ protected:
 protected:
     virtual RunnerConfig buildConfig() = 0;
     virtual void wireSpecificSignals(Test& t) {}
->>>>>>> Stashed changes
 
     Mpi& m_mpi;
     Registry& m_reg;
